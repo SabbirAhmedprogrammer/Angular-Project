545dd8c30ccf0735126147858cf81270681e759e
Julie Smyth <juliemcsmyth@gmail.com>
2020-05-21 10:36:06 -0400

    initial push with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "search-criteria", component: SearchCriteriaComponent },
+  { path: "", redirectTo: "/search-criteria", pathMatch: "full" },
+  { path: "**", redirectTo: "/search-criteria", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
