e9b6726d32417885b4ff8b1a0e81c6c9c9fdb2dd
Julie Smyth <juliemcsmyth@gmail.com>
2020-05-21 11:54:35 -0400

    adding componenents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { WatchlistPageComponent } from './watchlist-page/watchlist-page.component';
 
 
 const routes: Routes = [
-  { path: "search-criteria", component: SearchCriteriaComponent },
-  { path: "", redirectTo: "/search-criteria", pathMatch: "full" },
-  { path: "**", redirectTo: "/search-criteria", pathMatch: "full" }
+  { path: "main-page", component: MainPageComponent },
+  { path: "", redirectTo: "/main-page", pathMatch: "full" },
+  { path: "**", redirectTo: "/main-page", pathMatch: "full" },
+  { path: "watchlist-page", component: WatchlistPageComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { WatchlistPageComponent } from './watchlist-page/watchlist-page.component';
+import { HeaderComponent } from './header/header.component';
+import { PopularComponent } from './popular/popular.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchCriteriaComponent,
     MovieListComponent,
-    WatchlistPageComponent
+    WatchlistPageComponent,
+    HeaderComponent,
+    PopularComponent,
+    MainPageComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
